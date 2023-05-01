dfadb3cc8300c277e5714b0ef099b903e9557532
안호연 Henry Ahn <109224722+ahystroy@users.noreply.github.com>
2023-05-01 22:42:10 +0900

    Add files via upload

---
+++ CustomerManagement/datetime.h
@@ -0,0 +1,16 @@
+#ifndef __DATETIME_H__
+#define __DATETIME_H__
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define MAX_SIZE_STRING_DATETIME 20
+
+char current_datetime[MAX_SIZE_STRING_DATETIME];
+
+void update_datetime();
+int string_to_integer(char* string);
+void string_to_datetime(char* string_datetime, int* year, int* month, int* day, int* hour, int* minute, int* seconds);
+void sub_month_from_datetime(char* dst, char* string_datetime, int month);
+int string_datetime_comparison(char* str_dt1, char* str_dt2);
+
+#endif
---
+++ CustomerManagement/minigame_map.h
@@ -0,0 +1,16 @@
+#ifndef __MINIGAME_MAP_H__
+#define __MINIGAME_MAP_H__
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int*** maps;
+int map_size;
+int* row_sizes;
+int* col_sizes;
+
+void load_minigame_map();
+int** get_map_rb(int index, int *row_size, int *col_size);
+int** get_random_map_rb(int* row_size, int* col_size);
+void unload_minigame_map();
+
+#endif
